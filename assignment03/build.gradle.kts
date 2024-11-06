b287d0269a2c736c10387f2039f30781d19a9cfa
Chanwoo Kim <chanwoo040531@gmail.com>
2024-11-06 11:01:12 +0900

    Update build.gradle.kts

--- assignment03/build.gradle.kts
+++ assignment03/build.gradle.kts
@@ -1,6 +1,5 @@
 
 dependencies {
-    runtimeOnly("com.h2database:h2")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
